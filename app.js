3b099a95f77bf63d449ce3b24d3f5f6cdbc8dd9a
Ann Lampa <riann08@yahoo.com>
2020-11-26 22:13:10 +0100

    delete img

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let matchedCards = 0;
 let interval;
 let second = 0;
 let minute = 0;
-const timer = document.querySelector("#timer");
+
 
 //on load
 
@@ -41,8 +41,11 @@ function shuffle(c) {
 //copy all shuffled cards to the board
 
 const deckOfCards = document.querySelector("#main-section");
-//const shuffledCards = shuffle(cards);
-const shuffledCards = cards; //test set
+
+const shuffledCards = shuffle(cards);
+
+//test set
+// const shuffledCards = cards; 
 
 function startGame() {
   for (let i = 0; i < shuffledCards.length; i++) {
@@ -79,11 +82,15 @@ function moveCounter() {
   if ((move + 1) % 2 == 0 && move > 2) {
     let divCard3 = pickedDivCard[pickedDivCard.length - 3];
     let divCard2 = pickedDivCard[pickedDivCard.length - 2];
-    //let divCard1 = pickedDivCard[pickedDivCard.length - 1];
+
+    let card1 = pickedCards[pickedCards.length - 3];
+    let card2 = pickedCards[pickedCards.length - 2];
 
     setTimeout(() => {
-      divCard3.classList.remove("flip");
-      divCard2.classList.remove("flip");
+      if (card1 !== card2) {
+        divCard3.classList.remove("flip");
+        divCard2.classList.remove("flip");
+      }
     }, 250);
   }
 
@@ -109,17 +116,12 @@ function handleClick(evt) {
         alert("Congratulations! You found a match!");
       }, 300);
 
-      console.log(pickedCards);
-      console.log(card1);
-      console.log(card2);
-
-
       isFinished();
-
-      console.log(matchedCards);
     }
   }
 }
+
+const timer = document.querySelector("#timer");
 
 function startTimer() {
   interval = setInterval(function () {
@@ -138,4 +140,29 @@ function startTimer() {
 
 function stopTimer() {
   clearInterval(interval);
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
